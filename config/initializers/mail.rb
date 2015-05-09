8f48cebce293274c4f78be7eea1c424428f29141
nathanpena <nnpena@outlook.com>
2015-05-08 22:02:08 -0400

    Mail change

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -15,16 +15,4 @@ ActionMailer::Base.raise_delivery_errors = true
 # This interceptor just makes sure that local mail 
 # only emails you.
 # http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.to =  'nnpena@outlook.com'
-    message.cc = nil
-    message.bcc = nil
-  end
-end
 
-# Locally, outgoing mail will be 'intercepted' by the
-# above DevelopmentMailInterceptor before going out
-if Rails.env.development?
-  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
-end
