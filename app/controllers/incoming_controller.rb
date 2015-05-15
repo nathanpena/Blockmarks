2775238f95068402bca535d92c043b62464a8cab
nathanpena <nnpena@outlook.com>
2015-05-15 19:23:48 -0400

    create bookmark

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -10,8 +10,34 @@ class IncomingController < ApplicationController
 
     # You put the message-splitting and business
     # magic here. 
+    
+    # Find the user by using params[:sender]
+    @user = User.find(params[:sender])
+
+     # Find the topic by using params[:subject]
+     @topic = Topic.find(params[:subject])
+
+     # Assign the url to a variable after retreiving it from params["body-plain"]
+     @url = params[:"body-plain"]
+
+     # Check if user is nil, if so, create and save a new user
+     if @user.nil?
+        @user = User.create(:username => params[:from].slice(0..((:from.index('<')-2))), :email => params[:sender])
+        @user.save
+     end
+       
+
+     # Check if the topic is nil, if so, create and save a new topic
+     if @topic.nil?
+        @topic = Topic.create(:title => "Generic", :user_id => @user.id)
+     end
+
+     # Now that you're sure you have a valid user and topic, build and save a new bookmark
+     @bookmark = Bookmark.create(:url => @url, :topic => @topic.id)
+     @bookmark.save
 
     # Assuming all went well. 
     head 200
   end
+
 end
