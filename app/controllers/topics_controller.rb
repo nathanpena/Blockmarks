c8331fb7a046ce5ccf26544aa82d5efe1ed29b9b
nathanpena <nnpena@outlook.com>
2015-05-19 21:44:19 -0400

    Delete and Create topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,34 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user_id = current_user.id
+    @topic.save
+    redirect_to topics_path
+  end
+
+  def edit
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    redirect_to topics_path
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:title)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :topics
+
   get 'bookmark/index'
 
   devise_for :users
